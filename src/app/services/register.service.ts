0a562d7d201148e8f046f75127a3e1a63f0a459c
Kevin Agnew <kevin_2@live.co.uk>
2025-03-23 17:55:41 +0000

    Admin portal (#3)
    * Added admin portal module
    
    * login front end
    
    * Added admin register front end
    
    * Added initial request to pgadmin
    
    * Added bcrypt login
    
    * Added auth guard for routing

---
+++ src/app/services/register.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export type adminUser = {
+    firstname?: any,
+    lastname?: any,
+    email: any
+    password: any
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class RegisterService {
+
+    constructor(private http: HttpClient) { }
+
+    registerForAdmin(adminUser: adminUser): Observable<any> {
+        const params = {
+            firstname: adminUser.firstname,
+            lastname: adminUser.lastname,
+            email: adminUser.email,
+            password: adminUser.password
+        };
+        // If all checks out successfully register for admin
+        const apiUrl = 'http://localhost:3000/admin-register';
+        return this.http.post<any>(apiUrl, params);
+    }
+}
+
